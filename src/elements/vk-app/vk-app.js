3447a4ead1f9e06902c30d0bd9be2e76d38a9336
ewgeniux <ewgeniux@gmail.com>
2015-06-02 09:28:03 +0300

    upd

--- src/elements/vk-app/vk-app.js
+++ src/elements/vk-app/vk-app.js
@@ -3,7 +3,15 @@ Polymer({
 
   db: new PouchDB('db'),
 
+  _computeListWidth: function(isMobile) {
+    return isMobile ? '100%' : '320px';
+  },
+
+  _listTap: function() {
+    this.$.mainDrawerPanel.closeDrawer();
+  },
+
   ready: function() {
-    
+
   }
 });
